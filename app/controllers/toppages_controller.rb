3ed684a8ff89aef32f40bbe3c9f36b9e395e7883
tomohiro.kojima <niniseseko@gmail.com>
2021-09-18 01:27:22 +0000

    並び替え

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @boxpost = current_user.boxposts.build  # form_with 用
-      @boxposts = current_user.boxposts.order(id: :desc)
+      @boxposts = current_user.boxposts.order(content: :asc)
     end
   end
 end
