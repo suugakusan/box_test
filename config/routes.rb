c158b9fe067a82ac0495d30c89766250696eb6a7
tomohiro.kojima <niniseseko@gmail.com>
2021-09-18 07:24:30 +0000

    ゲストログイン実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
+  post 'guest_login', to: "guest_sessions#create"
   get 'signup', to: 'users#new'
   resources :users, only: [:create]
   resources :boxposts, only: [:create, :destroy]
