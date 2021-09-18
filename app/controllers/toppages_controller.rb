c8ffe7454d95a7dfb3768e2df677b2baba9b7a6d
tomohiro.kojima <niniseseko@gmail.com>
2021-09-18 05:10:03 +0000

    最小値以外

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,9 +3,36 @@ class ToppagesController < ApplicationController
     if logged_in?
       @boxpost = current_user.boxposts.build
       @boxposts = current_user.boxposts.order(content: :asc)
+      arry =current_user.boxposts.map {|hash| hash[:content].to_i}
+      sorted = arry.sort
       
+      if current_user.boxposts.any?
+      lowest = arry.min
+      highest = arry.max
+      len = arry.length
+      median = len % 2 == 1 ? sorted[len/2] : (sorted[len/2 - 1] + sorted[len/2]).to_f / 2
+      if len % 4 == 0
+        q1 = (sorted[len/4 - 1] + sorted[len/4]).to_f / 2
+      elsif len % 4 == 1
+        q1 = (sorted[len/4 - 1] + sorted[len/4]).to_f / 2
+      elsif len % 4 == 2
+        q1 = sorted[len/4]
+      elsif len % 4 == 3
+        q1 = sorted[len/4]
+      end
+
+      if len % 4 == 0
+        q3 = (sorted[len*3/4 - 1] + sorted[len*3/4]).to_f / 2
+      elsif len % 4 == 1
+        q3 = (sorted[len*3/4] + sorted[len*3/4 + 1]).to_f / 2
+      elsif len % 4 == 2
+        q3 = sorted[len*3/4]
+      elsif len % 4 == 3
+        q3 = sorted[len*3/4]
+      end
+      end
       category = ["データ"]
-      current_quantity = [@boxposts.pluck(:content)]
+      current_quantity = [[lowest,q1,median,q3,highest]]
 
       @graph = LazyHighCharts::HighChart.new('graph') do |f|
       f.title(text: '気温の箱ひげ図')
